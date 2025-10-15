c03f1ca8a6989dfdebe6826740ed038e2814022d
Vynz <76029153+VincelyVynz@users.noreply.github.com>
2025-10-15 17:30:12 +0530

    Add more routes

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from fastapi import FastAPI, HTTPException
-
+from datetime import datetime
 app = FastAPI()
 
 
 items = []
 @app.get('/')
-def root():
-    return "Hello"
+def home():
+    return {"msg": "Hello There!"}
 
-@app.post('/items')
-def post_items(item:str):
-    items.append(item)
-    return items
+@app.get('/greet/{name}')
+def greet(name:str):
+    return {'msg': f"Greetings {name}"}
 
-@app.get('/items')
-def get_items():
-    return items
+
+@app.get('/time')
+def tell_time():
+    time_now = datetime.now()
+    return {"Time": f"The time now is {time_now}"}
+
+
